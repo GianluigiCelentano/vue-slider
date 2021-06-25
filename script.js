59f4a99d0d4cc96a8ff347788c3d3ccdb3d6c652
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-25 16:58:43 +0200

    Vue Slider

---
+++ script.js
@@ -0,0 +1,40 @@
+Vue.config.devtools = true
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        images: [
+        'cane1.jpg',
+        'cane2.jpg',
+        'cane3.jpg',
+        'cane4.jpg'
+        ],
+    photoIndex: 0,
+    },
+    created() {
+        setInterval(() => {
+            this.nextImage()
+        }, 3000)
+    },
+    methods: {
+        nextImage: function(){
+            this.photoIndex++
+            if(this.photoIndex === (this.images.length -1)) {
+                this.photoIndex = 0
+            }
+        },
+        prevImage: function(){
+            this.photoIndex--
+            if(this.photoIndex === 0 -1) {
+                this.photoIndex = this.images.length -1
+            }
+        },
+        currentDot: function(index) {
+            if(index === this.photoIndex) {
+                return 'current'
+            } else {
+                return ''
+            }
+        }
+    }
+})
